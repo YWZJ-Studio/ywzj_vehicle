faf8b6861428f9c1c767854dbb256d23ca787345
Ranpoes <2213321305@qq.com>
2025-11-09 01:54:46 +0800

    乘客第三人称旋转插值

--- src/main/java/org/ywzj/vehicle/mixin/client/LivingEntityRendererMixin.java
+++ src/main/java/org/ywzj/vehicle/mixin/client/LivingEntityRendererMixin.java
@@ -19,9 +19,12 @@ public class LivingEntityRendererMixin<T extends LivingEntity> {
     @Inject(method = "setupRotations", at = @At("HEAD"), cancellable = true)
     public void setupRotations(T entity, PoseStack pPoseStack, float pAgeInTicks, float pRotationYaw, float pPartialTicks, CallbackInfo ci) {
         if (entity.getRootVehicle() != entity && entity.getRootVehicle() instanceof AbstractVehicle vehicle) {
-            float a = Mth.wrapDegrees(Mth.lerp(pPartialTicks, entity.yBodyRotO, entity.yBodyRot) - Mth.lerp(pPartialTicks, vehicle.yRotO, vehicle.getYRot()));
-            Quaternionf q = vehicle.rotYXZ();
-            q.rotateY((float) Math.toRadians(-a));
+            float yRotDiff = Mth.wrapDegrees(180 + pRotationYaw - Mth.lerp(pPartialTicks, vehicle.yRotO, vehicle.getYRot()));
+            Quaternionf q = new Quaternionf();
+            q.rotateY((float) Math.toRadians(Mth.lerp(pPartialTicks, -vehicle.yRotO, -vehicle.getYRot())))
+                    .rotateX((float) Math.toRadians(Mth.lerp(pPartialTicks, vehicle.xRotO, vehicle.getXRot())))
+                    .rotateZ((float) Math.toRadians(Mth.lerp(pPartialTicks, vehicle.zRotO, vehicle.getZRot())));
+            q.rotateY((float) Math.toRadians(-yRotDiff));
             Vector3f rot = new Vector3f();
             q.getEulerAnglesYXZ(rot);
             pPoseStack.mulPose(Axis.YP.rotation(rot.y));
