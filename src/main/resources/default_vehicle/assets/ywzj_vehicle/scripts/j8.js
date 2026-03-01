61c7668194bfb42223bf9b5bc90e2ca1be33f8be
Ranpoes <2213321305@qq.com>
2026-03-01 21:09:33 +0800

    追加歼-8战斗机

---
+++ src/main/resources/default_vehicle/assets/ywzj_vehicle/scripts/j8.js
@@ -0,0 +1,20 @@
+const missiles = ["missile_h1_1", "missile_h1_5", "missile_h1_2", "missile_h1_6", "missile_h1_3", "missile_h1_7", "missile_h1_4", "missile_h1_8"]
+
+function updateBones(context) {
+    // const previousPropellerRotation = context.getFloat("propellerRotation", 0);
+    // const propellerRotation = (previousPropellerRotation + context.getPower() / 5) % 360;
+    // context.setFloat("propellerRotation", propellerRotation)
+    //
+    // const builder = createPoseBuilder();
+    // builder.setRotation("propeller", 0, -propellerRotation, 0);
+    // builder.setRotation("propeller_tail", -propellerRotation * 5, 0, 0);
+    // builder.setRotation("mg", 0, -context.getPartYRot("auto_cannon"), 0);
+    // builder.setRotation("mg_up", context.getPartXRot("auto_cannon"), 0, 0);
+    // let remainMissiles = context.getWeaponRemainAmmo("sighting_system", 1)
+    // for (let i = 0; i < missiles.length; i++) {
+    //     if (i < missiles.length - remainMissiles) {
+    //         builder.hideBone(missiles[i])
+    //     }
+    // }
+    // return builder;
+}
