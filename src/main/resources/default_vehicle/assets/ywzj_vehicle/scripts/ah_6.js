eeb02f40bce24a86f0b9ea305507e6d348370f08
Ranpoes <2213321305@qq.com>
2026-04-22 20:06:24 +0800

    AH-6小鸟直升机动画脚本适配轴量方法

--- src/main/resources/default_vehicle/assets/ywzj_vehicle/scripts/ah_6.js
+++ src/main/resources/default_vehicle/assets/ywzj_vehicle/scripts/ah_6.js
@@ -2,22 +2,9 @@ function updateBones(context) {
     const previousPropellerRotation = context.getFloat("propellerRotation", 0)
     const propellerRotation = (previousPropellerRotation + context.getPower() / 15) % 360
     context.setFloat("propellerRotation", propellerRotation)
-
-    let d1 = 0
-    let d2 = 0
-    const control = context.getControlUnit()
-    if (control.left || control.right) {
-        d1 = control.left ? -10 : 10
-    }
-    if (control.forward || control.backward) {
-        d2 = control.forward ? 10 : -10
-    } else {
-        d2 = Math.max(-10, Math.min(10, (control.xRot - context.getLerpXRot()) / 30 * 10))
-    }
-
     const builder = createPoseBuilder()
     builder.setRotation("propeller", 0, -propellerRotation, 0)
     builder.setRotation("tailPropeller", -propellerRotation * 5, 0, 0)
-    builder.setRotation("control", -d2, 0, -d1)
+    builder.setRotation("control", context.getPitchInput() * 10, 0, -context.getRollInput() * 10)
     return builder
 }
