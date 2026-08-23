4b416cd3fef33ad42aa02698291d30b0ede56895
Ranpoes <2213321305@qq.com>
2026-08-23 22:14:05 +0800

    修多弹种武器站只从根武器站取备弹数问题

--- src/main/java/org/ywzj/vehicle/mixin/tacz/ShootKeyMixin.java
+++ src/main/java/org/ywzj/vehicle/mixin/tacz/ShootKeyMixin.java
@@ -1,7 +1,6 @@
 package org.ywzj.vehicle.mixin.tacz;
 
 import com.tacz.guns.client.input.ShootKey;
-import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.event.TickEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,17 +17,6 @@ public class ShootKeyMixin {
             at = @At(value = "HEAD"),
             cancellable = true)
     private static void autoShoot(TickEvent.ClientTickEvent event, CallbackInfo ci) {
-        AbstractVehicle.Seat seat = LocalVehiclePlayer.instance.seat;
-        if (seat != null && !seat.partUnit.getData().passengerCanUseItem()) {
-            ci.cancel();
-        }
-    }
-
-    @Inject(
-            method = "semiShoot",
-            at = @At(value = "HEAD"),
-            cancellable = true)
-    private static void semiShoot(InputEvent.MouseButton.Post event, CallbackInfo ci) {
         AbstractVehicle.Seat seat = LocalVehiclePlayer.instance.seat;
         if (seat != null && !seat.partUnit.getData().passengerCanUseItem()) {
             ci.cancel();
